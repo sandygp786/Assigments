92c0bf00072bf5f34ff6c57a07e797d8b3f0a321
sandygp786 <sandygp786@gmail.com>
2025-04-18 17:34:50 +0530

    Assignment1 and 2
    Assignment codes added

---
+++ Assignments1.py
@@ -0,0 +1,22 @@
+# Test 1: To Perform mathematical Operation on 2 user input Values
+a=input('Enter the first number:')
+a=int(a)
+b=input('Enter the Second number:')
+b=int(b)
+print(('\n') * 2)
+c=a+b
+print('Addition:',c)
+
+d=a-b
+print('Subtraction:',d)
+
+e=a*b
+print('Multiplication:',e)
+
+f=a/b
+print('Division:',f)
+
+
+
+
+
---
+++ Assignments2.py
@@ -0,0 +1,6 @@
+# Test 2: To print greetings for User input 1st and 2nd name. combine to Full name
+FNAME=input('Enter your first name:')
+LNAME=input('Enter your Last name:')
+print(('\n') * 2)
+CNAME=FNAME + " " + LNAME
+print("Hello, " + CNAME + "!" + " Welcome to the Python Program")
